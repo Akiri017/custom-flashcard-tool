7287e18ebcc1ca323373780ac9b44394ee770076
Ianne <mariannesantos174@gmail.com>
2025-02-17 22:30:29 +0800

    Set up project structure and implemented core classes

---
+++ src/test/java/io/custom/flashcard/tool/customFlashcardTest.java
@@ -0,0 +1,20 @@
+package io.custom.flashcard.tool;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class customFlashcardTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
